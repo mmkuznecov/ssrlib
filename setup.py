d1f7dd4fff5667f4b898b48a2421ef37e3ffb758
Mikhail Kuznetsov <mmkuznecov2002@gmail.com>
2026-05-11 20:37:57 +0300

    Bump version from 0.1.0 to 0.2.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="ssrlib",
-    version="0.1.0",
+    version="0.2.0",
     author="Mikhail Kuznetov",
     author_email="mmkuznecov2002@gmail.com",
     description="A modular Python framework for Self-Supervised Learning with automatic component discovery and intelligent caching",
